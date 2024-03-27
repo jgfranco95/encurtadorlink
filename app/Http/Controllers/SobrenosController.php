bc417af8176c690606b4dcb9a87d6bd8fba98989
jgfranco95 <jg.moura.franco@gmail.com>
2024-03-27 18:02:30 -0300

    oii

---
+++ app/Http/Controllers/SobrenosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SobrenosController extends Controller
+{
+    public function Sobrenos(){
+        return view('site.sobre-nos');
+     }
+}
